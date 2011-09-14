cf6c973262c57271b8935f448a6c20fb54d6459f
David Pilato <david@pilato.fr>
2011-09-14 15:38:19 +0200

    First version of the plugin. Tested only with lemonde.fr

---
+++ src/main/java/org/elasticsearch/river/rss/RssRiver.java
@@ -0,0 +1,298 @@
+/*
+ * Licensed to Elastic Search and Shay Banon under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Elastic Search licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.river.rss;
+
+import static org.elasticsearch.client.Requests.indexRequest;
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
+import org.elasticsearch.ExceptionsHelper;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.cluster.block.ClusterBlockException;
+import org.elasticsearch.common.UUID;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.joda.time.format.ISODateTimeFormat;
+import org.elasticsearch.common.util.concurrent.EsExecutors;
+import org.elasticsearch.common.xcontent.support.XContentMapValues;
+import org.elasticsearch.indices.IndexAlreadyExistsException;
+import org.elasticsearch.river.AbstractRiverComponent;
+import org.elasticsearch.river.River;
+import org.elasticsearch.river.RiverName;
+import org.elasticsearch.river.RiverSettings;
+import org.elasticsearch.river.rss.support.Feed;
+import org.elasticsearch.river.rss.support.FeedMessage;
+import org.elasticsearch.river.rss.support.RssFeedParser;
+
+/**
+ * @author dadoonet (David Pilato)
+ */
+public class RssRiver extends AbstractRiverComponent implements River {
+
+	private final Client client;
+
+	private final String url;
+
+	private final String indexName;
+
+	private final String typeName;
+
+	private volatile Thread thread;
+
+	private volatile boolean closed = false;
+
+	private final int updateRate;
+
+	private final RssFeedParser parser;
+	
+	@SuppressWarnings({ "unchecked" })
+	@Inject
+	public RssRiver(RiverName riverName, RiverSettings settings, Client client)
+			throws MalformedURLException {
+		super(riverName, settings);
+		this.client = client;
+
+		if (settings.settings().containsKey("rss")) {
+			Map<String, Object> rssSettings = (Map<String, Object>) settings.settings().get("rss");
+			url = XContentMapValues.nodeStringValue(rssSettings.get("url"), null);
+			updateRate  = XContentMapValues.nodeIntegerValue(rssSettings.get("update_rate"), 15 * 60 * 1000);
+		} else {
+			url = "http://www.lemonde.fr/rss/une.xml";
+			logger.warn("You didn't define the rss url. Switching to defaults : [{}]", url);
+			updateRate = 15 * 60 * 1000;
+		}
+
+		if (logger.isInfoEnabled()) logger.info("creating rss stream river for [{}]", url);
+		
+		this.parser = new RssFeedParser(url);
+
+		if (settings.settings().containsKey("index")) {
+			Map<String, Object> indexSettings = (Map<String, Object>) settings
+					.settings().get("index");
+			indexName = XContentMapValues.nodeStringValue(
+					indexSettings.get("index"), riverName.name());
+			typeName = XContentMapValues.nodeStringValue(
+					indexSettings.get("type"), "page");
+		} else {
+			indexName = riverName.name();
+			typeName = "page";
+		}
+	}
+
+	@Override
+	public void start() {
+		if (logger.isInfoEnabled()) logger.info("Starting rss stream");
+		try {
+			client.admin().indices().prepareCreate(indexName).execute()
+					.actionGet();
+		} catch (Exception e) {
+			if (ExceptionsHelper.unwrapCause(e) instanceof IndexAlreadyExistsException) {
+				// that's fine
+			} else if (ExceptionsHelper.unwrapCause(e) instanceof ClusterBlockException) {
+				// ok, not recovered yet..., lets start indexing and hope we
+				// recover by the first bulk
+				// TODO: a smarter logic can be to register for cluster event
+				// listener here, and only start sampling when the block is
+				// removed...
+			} else {
+				logger.warn("failed to create index [{}], disabling river...",
+						e, indexName);
+				return;
+			}
+		}
+		thread = EsExecutors.daemonThreadFactory(settings.globalSettings(),
+				"rss_slurper").newThread(new RSSParser());
+		thread.start();
+	}
+
+	@Override
+	public void close() {
+		if (logger.isInfoEnabled()) logger.info("Closing rss river");
+		closed = true;
+		if (thread != null) {
+			thread.interrupt();
+		}
+	}
+
+	
+	public static final SimpleDateFormat rfc822DateFormats[] = new SimpleDateFormat[] { 
+		new SimpleDateFormat("EEE, d MMM yy HH:mm:ss z", Locale.US), 
+		new SimpleDateFormat("EEE, d MMM yy HH:mm z", Locale.US), 
+		new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z", Locale.US), 
+		new SimpleDateFormat("EEE, d MMM yyyy HH:mm z", Locale.US), 
+		new SimpleDateFormat("d MMM yy HH:mm z", Locale.US), 
+		new SimpleDateFormat("d MMM yy HH:mm:ss z", Locale.US), 
+		new SimpleDateFormat("d MMM yyyy HH:mm z", Locale.US), 
+		new SimpleDateFormat("d MMM yyyy HH:mm:ss z", Locale.US)};
+	
+	public static Date parseRfc822(String rfcDate) throws ParseException {
+		for (int i = 0; i < rfc822DateFormats.length; i++) {
+			try {
+				Date feedDate = rfc822DateFormats[i].parse(rfcDate);
+				return feedDate;
+			} catch (ParseException e) {
+			}
+		}
+		throw new ParseException("Can not parse RFC822 date", 0);
+	}
+	
+	
+	
+	private class RSSParser implements Runnable {
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public void run() {
+
+			while (true) {
+				if (closed) {
+					return;
+				}
+				
+				// Let's call the Rss flow
+				Feed feed = parser.readFeed();
+				String strDate = feed.getPubDate();
+				logger.debug("Feed publish date is {}", strDate);
+
+				Date feedDate = null;
+				if (strDate != null) {
+					try {
+						feedDate = parseRfc822(strDate);
+					} catch (ParseException e) {
+						logger.warn("Can not parse date {}", strDate);
+					}
+				}
+
+				Date lastDate = null;
+				try {
+					// Do something
+					if (logger.isDebugEnabled()) logger.debug("Starting to parse RSS feed");
+					client.admin().indices().prepareRefresh("_river").execute().actionGet();
+					GetResponse lastSeqGetResponse =
+							client.prepareGet("_river", riverName().name(), "_lastupdate").execute().actionGet();
+					if (lastSeqGetResponse.exists()) {
+						Map<String, Object> rssState = (Map<String, Object>) lastSeqGetResponse
+								.sourceAsMap().get("rss");
+						
+						if (rssState != null) {
+							String strLastDate = rssState.get("_lastupdate").toString();
+							lastDate = ISODateTimeFormat.dateOptionalTimeParser().parseDateTime(strLastDate).toDate();
+						}
+					} else {
+						// First call
+						if (logger.isDebugEnabled()) logger.debug("_lastupdate doesn't exist");
+					}
+				} catch (Exception e) {
+					logger.warn("failed to get _lastupdate, throttling....", e);
+				}
+
+				// Comparing dates to see if we have something to do or not
+				if (lastDate == null || (feedDate != null && feedDate.after(lastDate))) {
+					// We have to send results to ES
+					if (logger.isDebugEnabled()) logger.debug("Feed is updated : {}", feed);
+					
+	                BulkRequestBuilder bulk = client.prepareBulk();
+                    try {
+                    	// We have now to send each feed to ES
+                		for (FeedMessage message : feed.getMessages()) {
+                			// Let's define the rule for UUID generation
+                			String id = UUID.nameUUIDFromBytes(message.getDescription().getBytes()).toString();
+                			
+                			// Let's look if object already exists
+        					GetResponse oldMessage =
+        							client.prepareGet(indexName, typeName, id).execute().actionGet();
+                			if (!oldMessage.exists()) {
+                                bulk.add(indexRequest(indexName).type(typeName).id(id)
+                                        .source(jsonBuilder()
+                                        	.startObject()
+                                        		.field("title", message.getTitle())
+                                        		.field("author", message.getAuthor())
+                                        		.field("description", message.getDescription())
+                                        		.field("link", message.getLink())
+                                        	.endObject()));
+                    			
+                    			
+                                if (logger.isDebugEnabled()) logger.debug("FeedMessage is updated : {}", message);
+                			} else {
+                				if (logger.isTraceEnabled()) logger.trace("FeedMessage {} already exist. Ignoring", id);
+                			}
+        					
+        					
+                		}
+                    	
+                    	
+                        if (logger.isTraceEnabled()) {
+                            logger.trace("processing [_seq  ]: [{}]/[{}]/[{}], last_seq [{}]", indexName, riverName.name(), "_lastupdate", feedDate);
+                        }
+                        // We store the lastupdate date
+                        bulk.add(indexRequest("_river").type(riverName.name()).id("_lastupdate")
+                                .source(jsonBuilder().startObject().startObject("rss").field("_lastupdate", feedDate).endObject().endObject()));
+                    } catch (IOException e) {
+                        logger.warn("failed to add feed message entry to bulk indexing");
+                    }
+
+	                try {
+	                    BulkResponse response = bulk.execute().actionGet();
+	                    if (response.hasFailures()) {
+	                        // TODO write to exception queue?
+	                        logger.warn("failed to execute" + response.buildFailureMessage());
+	                    }
+	                } catch (Exception e) {
+	                    logger.warn("failed to execute bulk", e);
+	                }
+					
+				} else {
+					// Nothing new... Just relax !
+					if (logger.isDebugEnabled()) logger.debug("Nothing new in the feed... Relaxing...");
+				}
+				
+				
+				
+				try {
+					if (logger.isDebugEnabled()) logger.debug("Rss river is going to sleep for {} ms", updateRate);
+					Thread.sleep(updateRate);
+					continue;
+				} catch (InterruptedException e1) {
+					if (closed) {
+						return;
+					}
+				}
+
+				
+				try {
+				} catch (Exception e) {
+					if (closed) {
+						return;
+					}
+					logger.error("failed to parse stream", e);
+				}
+			}
+		}
+	}
+}
---
+++ src/test/java/org/elasticsearch/river/rss/RssRiverTest.java
@@ -0,0 +1,120 @@
+/*
+ * Licensed to Elastic Search and Shay Banon under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Elastic Search licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.river.rss;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.indices.IndexMissingException;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
+
+/**
+ * @author dadoonet (David Pilato)
+ */
+public class RssRiverTest {
+
+	public static void main(String[] args) throws Exception {
+		String url = "http://www.lemonde.fr/rss/une.xml";
+
+		Node node = NodeBuilder
+				.nodeBuilder()
+				.settings(
+						ImmutableSettings.settingsBuilder()
+						.put("gateway.type", "local")
+						.put("path.data", "./target/es/data")		
+						.put("path.logs", "./target/es/logs")		
+						.put("path.work", "./target/es/work")		
+						).node();
+		
+		// We wait for one second to let ES start
+		Thread.sleep(1000);
+		try {
+			node.client().admin().indices()
+					.delete(new DeleteIndexRequest("_river")).actionGet();
+			// We wait for one second to let ES delete the river
+			Thread.sleep(1000);
+		} catch (IndexMissingException e) {
+			// Index does not exist... Fine
+		}
+
+		try {
+			node.client().admin().indices()
+					.delete(new DeleteIndexRequest("rss")).actionGet();
+			// We wait for one second to let ES delete the index
+			Thread.sleep(1000);
+		} catch (IndexMissingException e) {
+			// Index does not exist... Fine
+		}
+
+		// Creating the index with some mapping
+		node.client().admin().indices().create(new CreateIndexRequest("rss")).actionGet();
+
+		XContentBuilder xbMapping = jsonBuilder()
+				.startObject()
+					.startObject("page")
+						.startObject("properties")
+							.startObject("title")
+								.field("type", "string")
+								.field("analyzer", "french")
+							.endObject()
+							.startObject("description")
+								.field("type", "string")
+								.field("analyzer", "french")
+							.endObject()
+							.startObject("author")
+								.field("type", "string")
+								.field("analyzer", "french")
+							.endObject()
+							.startObject("link")
+								.field("type", "string")
+							.endObject()
+						.endObject()
+					.endObject()
+				.endObject();
+		
+		node.client().admin().indices()
+			.preparePutMapping("rss")
+			.setType("page")
+			.setSource(xbMapping)
+			.execute().actionGet();
+		
+		// We create a rss feed on lemonde with a refresh every ten minutes
+		// int updateRate = 10 * 60 * 1000;
+		int updateRate = 10 * 1000;
+		XContentBuilder xb = jsonBuilder()
+				.startObject()
+					.field("type", "rss")
+					.startObject("rss")
+						.field("url", url)
+						.field("update_rate", updateRate)
+					.endObject()
+				.endObject();
+
+		node.client().prepareIndex("_river", "rss", "_meta").setSource(xb)
+				.execute().actionGet();
+
+		// Let's wait one hour 
+		Thread.sleep(1 * 60 * 60 * 1000);
+	}
+}
